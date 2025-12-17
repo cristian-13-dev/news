fd63cb0b802c6eb75a38c7784f42ea4ab753f3b2
Eravity <cebotaricristian92@gmail.com>
2025-12-17 14:32:39 +0200

    refactor: streamline ESLint configuration by removing globalIgnores and restructuring extends

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,18 @@
-import { defineConfig, globalIgnores } from "eslint/config";
+import { defineConfig } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals.js";
 import nextTs from "eslint-config-next/typescript.js";
 
-const eslintConfig = defineConfig([
-  nextVitals,
-  nextTs,
-  globalIgnores([
+const eslintConfig = defineConfig({
+  extends: [
+    ...(Array.isArray(nextVitals.extends) ? nextVitals.extends : []),
+    ...(Array.isArray(nextTs.extends) ? nextTs.extends : []),
+  ],
+  ignorePatterns: [
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
-  ]),
-]);
+  ],
+});
 
 export default eslintConfig;
